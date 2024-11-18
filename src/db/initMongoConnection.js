307322ebd87e313af93374f40f17259314e7a492
Yarik2019 <yroslavterno@gmail.com>
2024-11-18 17:15:45 +0200

    add code

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -11,13 +11,6 @@ export const initMongoConnection = async () => {
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
-    // const user = 'ciba873';
-    // const pwd = 'RXY9w47Lu8rFJqZb';
-    // const url = 'contacts.5yvgg.mongodb.net';
-    // const db = 'contacts';
-    // await mongoose.connect(
-    //   `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
-    // );
 
     console.log('Mongo connection successfully established!');
   } catch (error) {
