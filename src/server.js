849447b6efb3af7af301f8bd5c7654c6646b374b
Yarik2019 <yroslavterno@gmail.com>
2024-11-24 18:31:03 +0200

    add tests

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ import pinoHttp from 'pino-http';
 import pino from 'pino';
 import cors from 'cors';
 
-// import ContactsRouter from './routers/contacts.js';
-import {
-  deleteContact,
-  getAllContcats,
-  getContactById,
-  patchContact,
-  postContact,
-} from './services/contacts.js';
+import ContactsRouter from './routers/contacts.js';
+// import {
+//   deleteContact,
+//   getAllContcats,
+//   getContactById,
+//   patchContact,
+//   postContact,
+// } from './services/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 const PORT = process.env.PORT || 3000;
@@ -27,76 +27,76 @@ export const setupServer = () => {
   app.use(cors());
   app.use(pinoHttp({ logger }));
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContcats();
+  // app.get('/contacts', async (req, res) => {
+  //   const contacts = await getAllContcats();
 
-    res.send({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  //   res.send({
+  //     status: 200,
+  //     message: 'Successfully found contacts!',
+  //     data: contacts,
+  //   });
+  // });
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
+  // app.get('/contacts/:contactId', async (req, res, next) => {
+  //   const { contactId } = req.params;
 
-    const contact = await getContactById(contactId);
+  //   const contact = await getContactById(contactId);
 
-    if (!contact) {
-      return res
-        .status(404)
-        .send({ status: 404, message: 'Contact not found' });
-    }
+  //   if (!contact) {
+  //     return res
+  //       .status(404)
+  //       .send({ status: 404, message: 'Contact not found' });
+  //   }
 
-    res.send({
-      status: 200,
-      message: `Successfully found contact with id: ${contactId}`,
-      data: contact,
-    });
-  });
-  app.post('/contacts', async (req, res) => {
-    const contacts = await postContact(req.body);
+  //   res.send({
+  //     status: 200,
+  //     message: `Successfully found contact with id: ${contactId}`,
+  //     data: contact,
+  //   });
+  // });
+  // app.post('/contacts', async (req, res) => {
+  //   const contacts = await postContact(req.body);
 
-    res.send({
-      status: 200,
-      message: 'Successfully created contacts!',
-      data: contacts,
-    });
-  });
+  //   res.send({
+  //     status: 200,
+  //     message: 'Successfully created contacts!',
+  //     data: contacts,
+  //   });
+  // });
 
-  app.delete('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
+  // app.delete('/contacts/:contactId', async (req, res, next) => {
+  //   const { contactId } = req.params;
 
-    const contact = await deleteContact(contactId);
+  //   const contact = await deleteContact(contactId);
 
-    if (!contact) {
-      return res
-        .status(404)
-        .send({ status: 404, message: 'Contact not found' });
-    }
+  //   if (!contact) {
+  //     return res
+  //       .status(404)
+  //       .send({ status: 404, message: 'Contact not found' });
+  //   }
 
-    res.sendStatus(204);
-  });
+  //   res.sendStatus(204);
+  // });
 
-  app.patch('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
+  // app.patch('/contacts/:contactId', async (req, res, next) => {
+  //   const { contactId } = req.params;
 
-    const contact = await patchContact(contactId, req.body);
+  //   const contact = await patchContact(contactId, req.body);
 
-    if (!contact) {
-      return res
-        .status(404)
-        .send({ status: 404, message: 'Contact not found' });
-    }
+  //   if (!contact) {
+  //     return res
+  //       .status(404)
+  //       .send({ status: 404, message: 'Contact not found' });
+  //   }
 
-    res.send({
-      status: 200,
-      message: 'Successfully patched a contact!',
-      data: contact,
-    });
-  });
+  //   res.send({
+  //     status: 200,
+  //     message: 'Successfully patched a contact!',
+  //     data: contact,
+  //   });
+  // });
 
-  // app.use(ContactsRouter);
+  app.use(ContactsRouter);
   // app.use(ContactsRouter);
   app.use('*', notFoundHandler);
 
