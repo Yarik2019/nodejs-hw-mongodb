7df63717b89f51e416915ce02c35a549c5be6d07
Yarik2019 <yroslavterno@gmail.com>
2024-12-18 00:07:46 +0200

    add temp

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve('tmp'));
+    cb(null, path.resolve('temp'));
   },
   filename: function (req, file, cb) {
     const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
