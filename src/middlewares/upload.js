ae4f7fe3e9047490c1b7b15aa22cc31dd952df1e
Yarik2019 <yroslavterno@gmail.com>
2024-12-18 00:01:07 +0200

    add

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,9 +1,11 @@
+import path from 'node:path';
+
 import multer from 'multer';
-import { TEMP_UPLOAD_DIR } from '../constants/constants.js';
+import { TEMP_UPLOAD_DIR } from '../constants/constants';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, TEMP_UPLOAD_DIR);
+    cb(null, path.resolve('src', 'tmp'));
   },
   filename: function (req, file, cb) {
     const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
