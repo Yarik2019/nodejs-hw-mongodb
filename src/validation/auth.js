b80f9bca90fa2c3f80f6454d4f6987db1a1027de
Yarik2019 <yroslavterno@gmail.com>
2024-12-21 17:42:15 +0200

    add loginOrSignupWithGoogle

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -19,3 +19,7 @@ export const resetPasswordSchema = Joi.object({
   token: Joi.string().required(),
   password: Joi.string().min(3).max(20).required(),
 });
+
+export const loginWithGoogleOAuthSchema = Joi.object({
+  code: Joi.string().required(),
+});
